b9ef81ca1f2e2ba306ee2e17c5e0ac94048a1e0b
Nailson97 <nailsonsousa97@gmail.com>
2023-01-02 06:54:00 -0300

    feat: add responsiveness to the header using chackraui

--- src/components/Form/Header/Profile.tsx
+++ src/components/Form/Header/Profile.tsx
@@ -1,19 +1,25 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
-    return (
-        <Flex align="center">
-          <Box mr="4" textAlign="right">
-            <Text>Nailson Soares</Text>
-            <Text color="gray.300" fontSize="small">
-              nailsonsoares45@gmail.com
-            </Text>
-          </Box>
-          <Avatar
-            size="md"
-            name="Nailson Soares"
-            src="https://github.com/Nailson97.png"
-          />
-        </Flex>
-    )
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
+  return (
+    <Flex align="center">
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Nailson Soares</Text>
+          <Text color="gray.300" fontSize="small">
+            nailsonsoares45@gmail.com
+          </Text>
+        </Box>
+      )}
+      <Avatar
+        size="md"
+        name="Nailson Soares"
+        src="https://github.com/Nailson97.png"
+      />
+    </Flex>
+  );
 }
