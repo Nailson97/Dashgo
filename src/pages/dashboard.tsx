68bd2c802002efbe6033f43737d1c9363d2b7a7f
Nailson97 <nailsonsousa97@gmail.com>
2023-01-02 17:25:13 -0300

    feat: adding responsiveness to sidebar and dashboard using chackraui

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -70,23 +70,24 @@ export default function Dashboard() {
       <Header />
       <Flex w="100%" my="6" maxWidth={1280} mx="auto" px="6">
         <Sidebar />
+
         <SimpleGrid
           flex="1"
           gap="4"
           minChildWidth="320px"
           alignItems="flex-start"
         >
-          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+          <Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
               Inscritos da semana
             </Text>
-            <Chart options={options} series={series} type="area" height={160} width={400} />
+            <Chart options={options} series={series} type="area" height={160} width={300} />
           </Box>
-          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+          <Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
               Taxa de abertura
             </Text>
-            <Chart options={options} series={series} type="area" height={160} width={400}/>
+            <Chart options={options} series={series} type="area" height={160} width={300}/>
           </Box>
         </SimpleGrid>
       </Flex>
