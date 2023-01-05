9433abc3d2d0f85913da67a35200dd38ae0f94e0
Nailson97 <nailsonsousa97@gmail.com>
2023-01-05 14:03:09 -0300

    feat: adding responsiveness to the user registration

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,31 +1,36 @@
-import { Box,Divider,Flex, Heading, SimpleGrid, HStack, Button } from "@chakra-ui/react";
+import { Box,Divider,Flex, Heading, SimpleGrid, Stack, HStack, Button, useBreakpointValue } from "@chakra-ui/react";
 import { Header } from "../../components/Form/Header";
 import { Sidebar } from "../../components/Form/Header/Sidebar";
 import { Input } from "../../components/Form/Input";
 
   export default function CreateUser() {
+    const isWideVersion = useBreakpointValue({
+      base: false,
+      lg: true
+    })
+
     return (
       <Box>
         <Header />
         <Flex w="100%" my="6" maxWidth={1280} mx="auto" px="6">
           <Sidebar />
 
-          <Box flex="1" borderRadius={8} bgColor="gray.800" p="8">
+          <Box flex="1" borderRadius={8} bgColor="gray.800" p={["6", "8"]}>
             <Heading size="lg" fontWeight="normal">Criar usuário</Heading>
 
             <Divider my="6" borderColor="gray.700" />
 
-            <HStack spacing="8">
-               <SimpleGrid minChildWidth="240" spacing="8" w="100%">
+            <Stack direction={!isWideVersion ? "column": 'row' } spacing="8">
+               <SimpleGrid minChildWidth="240" spacing={["6", "8"]} w="100%">
                   <Input name="name" label="Nome completo" />  
-                  <Input name="password" type="password" label="Senha" />  
+                 {isWideVersion ? <Input name="password" type="password" label="Senha" /> : <Input name="email" type="email" label="E-mail" /> }
                </SimpleGrid> 
 
-               <SimpleGrid minChildWidth="240" spacing="8" w="100%">
-                  <Input name="email" type="email" label="E-mail" /> 
+               <SimpleGrid minChildWidth="240" spacing={["6", "8"]} w="100%">
+                  {isWideVersion ?  <Input name="email" type="email" label="E-mail" /> : <Input name="password" type="password" label="Senha" /> } 
                   <Input name="password_confirmation" type="password" label="Confirmação da senha" />  
                </SimpleGrid> 
-            </HStack>
+            </Stack>
 
             <Flex mt="8" justify="flex-end">
                <HStack spacing="4">
