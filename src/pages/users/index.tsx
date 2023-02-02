da38241f035f266e8e8be8b15441f1d3bf485c37
Nailson97 <nailsonsousa97@gmail.com>
2023-02-02 13:19:41 -0300

    feat: adding data refetch flag

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -31,11 +31,10 @@ interface userProps {
 }
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery("users", async () => {
+  const { data, isLoading, isFetching, error } = useQuery("users", async () => {
     const response = await fetch("http://localhost:3000/api/users");
     const data = await response.json();
 
-   
     const users = data.users.map((user: userProps ) => {
       return {
         id: user.id,
@@ -69,6 +68,7 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" alignItems="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              { !isLoading && isFetching && <Spinner size="sm" color='gray.500' ml="4" />}
             </Heading>
 
             <Button
